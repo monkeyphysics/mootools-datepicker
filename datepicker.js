99310c4211c3f03b34cc48a5b13c9f02176bbde2
Enrique Erne <enrique.erne@gmail.com>
2009-11-29 13:41:11 +0100

    fixed bug button previous month disappeared on the last month

--- datepicker.js
+++ datepicker.js
@@ -418,7 +418,7 @@ var DatePicker = new Class({
 				e.addClass('unavailable');
 				if (available) {
 					this.limit.right = true;
-				} else if (this.d.getMonth() == month) {
+				} else if (this.d.getMonth() < month) {
 					this.limit.left = true;
 				}
 			} else {
