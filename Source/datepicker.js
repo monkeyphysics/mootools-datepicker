724e5e9ac65c0e43aa84e05018305d3e7f756df6
Arian <arian@aryweb.nl>
2010-04-17 20:08:07 +0200

    Fix when date was 0 (jan)

--- Source/datepicker.js
+++ Source/datepicker.js
@@ -730,7 +730,7 @@ Date.extend({
 		d.setDate(1);
 		['year', 'month', 'day', 'hours', 'minutes', 'seconds'].each(function(type) {
 			var v = values[type];
-			if (!v) return;
+			if (!$chk(v)) return;
 			switch (type) {
 				case 'day': d.setDate(v); break;
 				case 'month': d.setMonth(v); break;
