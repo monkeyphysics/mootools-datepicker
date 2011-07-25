7f76b92e89df91f515f9af7b65f3c1aba673bb4a
Arian <arian@aryweb.nl>
2011-07-25 13:30:49 +0100

    Some cleanups for the aria changes

--- Source/Picker.js
+++ Source/Picker.js
@@ -62,14 +62,14 @@ var Picker = new Class({
 		var header = this.header = new Element('div.header').inject(picker);
 
 		var title = this.title = new Element('div.title').inject(header);
-		this.titleId = 'pickertitle-'+String.uniqueID();
+		var titleID = this.titleID = 'pickertitle-' + String.uniqueID();
 		this.titleText = new Element('div', {
-			role: 'heading',
+			'role': 'heading',
 			'class': 'titleText',
-			id: this.titleId,
+			'id': titleID,
 			'aria-live': 'assertive',
 			'aria-atomic': 'true'
-			}).inject(title);
+		}).inject(title);
 
 		this.closeButton = new Element('div.closeButton[text=x][role=button]')
 			.addEvent('click', this.close.pass(false, this))
@@ -128,8 +128,7 @@ var Picker = new Class({
 	open: function(noFx){
 		if (this.opened == true) return this;
 		this.opened = true;
-		var picker = this.picker.setStyle('display', 'block');
-		picker.set('aria-hidden', 'false');
+		var picker = this.picker.setStyle('display', 'block').set('aria-hidden', 'false')
 		if (this.shim) this.shim.show();
 		this.fireEvent('open');
 		if (this.options.useFadeInOut && !noFx){
@@ -150,8 +149,7 @@ var Picker = new Class({
 		this.opened = false;
 		this.fireEvent('close');
 		var self = this, picker = this.picker, hide = function(){
-			picker.setStyle('display', 'none');
-			picker.set('aria-hidden', 'true');
+			picker.setStyle('display', 'none').set('aria-hidden', 'true');
 			if (self.shim) self.shim.hide();
 			self.fireEvent('hide');
 		};
