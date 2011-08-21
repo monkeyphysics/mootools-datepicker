a31a482af32420e061318df0efdeeabbc3f5ca45
Arian Stolwijk <arian@aryweb.nl>
2011-08-21 16:38:21 +0300

    Fixes #68 - add missing comma in French Locale.

--- Source/Locale.fr-FR.DatePicker.js
+++ Source/Locale.fr-FR.DatePicker.js
@@ -11,7 +11,7 @@ provides: Locale.fr-FR.DatePicker
 Locale.define('fr-FR', 'DatePicker', {
 	select_a_time: 'Choisir l\'heure',
 	use_mouse_wheel: 'Utiliser la molette pour changer l\'heure rapidement',
-	time_confirm_button: 'OK'
+	time_confirm_button: 'OK',
 	apply_range: 'Appliquer',
 	cancel: 'Annuler',
 	week: 'Sem'
